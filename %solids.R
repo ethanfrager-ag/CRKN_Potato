8ff4f2c0abf9e2a6c6f2ca303ec0f912bb57533a
ethanfrager-ag <ethanmfrager@gmail.com>
2026-08-04 09:08:28 -0700

    Update %solids.R

--- %solids.R
+++ %solids.R
@@ -1,10 +1,10 @@
 data2023 <- read.csv("~/Documents/usda potato breeding/data_analysis/raw_data/gravity2023/combined.csv", stringsAsFactors = FALSE)     
 
 
-m = -214.9206
-b = 218.1852
+b = -214.9206
+m = 218.1852
 
-data2023$solids <- (b*data2023$SpecificGravity)+m
+data2023$solids <- (m*data2023$SpecificGravity)+b
 
 write.csv(data2023, "combined.csv", row.names = FALSE)
 
