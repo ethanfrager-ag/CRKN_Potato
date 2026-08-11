2ed2f1370704cb73df4a60e5867542dbbe865abb
ethanfrager-ag <ethanmfrager@gmail.com>
2026-08-11 14:05:24 -0700

    Update %solids.R

--- %solids.R
+++ %solids.R
@@ -1,11 +1,25 @@
-data2023 <- read.csv("~/Documents/usda potato breeding/data_analysis/raw_data/gravity2023/combined.csv", stringsAsFactors = FALSE)     
+weightsag <- read.csv("~/Documents/usda potato breeding/data_analysis/clean_data/combined_2024.csv", stringsAsFactors = FALSE)     
 
 
 b = -214.9206
 m = 218.1852
 
-data2023$solids <- (m*data2023$SpecificGravity)+b
+weightsag$solids <- (m*weightsag$Gravity)+b
 
-write.csv(data2023, "combined.csv", row.names = FALSE)
+write.csv(weightsag, "agdata.csv", row.names = FALSE)
 
 #equation from: Kleinkopf, G. E., et al. "Specific gravity of Russet Burbank potatoes." American potato journal 64.11 (1987): 579-587.
+
+#recalculate SG from wet and dry weight 
+setwd("~/Documents/usda potato breeding/data_analysis/clean_data")
+
+weightsag$Gravity <- (weightsag$DryWeight/(weightsag$DryWeight-weightsag$WetWeight))
+
+write.csv(weightsag, "agdata.csv", row.names = FALSE)
+
+#find total weight of starch 
+
+weightsag$TotalStarchGrams <- (((0.01*weightsag$solids)*weightsag$DryWeight)*1000)
+write.csv(weightsag, "agdata.csv", row.names = FALSE)
+
+
